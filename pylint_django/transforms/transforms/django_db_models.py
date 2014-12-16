2a05b24c781173dda3959a7ccfd9cb6d36e6e539
Artur Gavkaliuk <mandeburka@gmail.com>
2014-12-16 14:20:37 +0200

    add django model manager transform

--- pylint_django/transforms/transforms/django_db_models.py
+++ pylint_django/transforms/transforms/django_db_models.py
@@ -1,3 +1,5 @@
+from django.db.models.query import QuerySet
+
 class Model(object):
     _meta = None
     objects = None
@@ -7,3 +9,6 @@ class Model(object):
 
     MultipleObjectsReturned = None
     DoesNotExist = None
+
+class Manager(QuerySet):
+    pass
